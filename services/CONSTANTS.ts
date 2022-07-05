71ebd9f31596b25c9347485b5d68ec85615b76ec
osama bin junaid <osama.junaid@c3m.io>
2022-07-05 18:06:52 +0530

    added todo fetching support

--- services/CONSTANTS.ts
+++ services/CONSTANTS.ts
@@ -1,2 +1,1 @@
-export const DB_SERVICES= 'http://localhost:4000';
-// export const DB_SERVICES= 'https://eubrics-project.herokuapp.com';
+export const DB_SERVICES= 'https://eubrics-project.herokuapp.com';
